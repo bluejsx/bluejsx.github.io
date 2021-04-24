2e942c59a7e3afee879024612034a02aecd671c0
Momijiichigo <ichigomomiji436@gmail.com>
2021-04-24 15:11:11 +0900

    update

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,14 @@
 import '@vanillajsx/vjsx'
 import marked from 'marked'
 import hljs from 'highlight.js'
-
+import '../declaration.d'
 import 'github-markdown-css'
 import 'highlight.js/styles/vs2015.css'
 import { main } from './Main.module.scss'
 import './container.scss'
 
 import Example from './Example'
-import article from './article.md?raw'
+//import article from './article.md?raw'
 
 const {log} = console
 
@@ -19,13 +19,18 @@ marked.setOptions({
 })
 const Main = () =>{
   const container = (<div class='container'>
-    {
-      marked(article).split('<hr>').map(htmStr=>(
-        <section innerHTML={htmStr} class='markdown-body' />
-      ))
-    }
+    {async (set: any, elem: VJSX.JSX.Element)=>{
+      await import('./article.md?raw').then(mod=>
+        marked(mod.default).split('<hr>').forEach(htmStr=>
+          elem.appendChild(
+            <section innerHTML={htmStr} class='markdown-body' />
+          )
+        )
+      )
+      container.querySelector('#example-result-space').appendChild(<Example />)
+    }}
   </div>)
-  container.querySelector('#example-result-space').appendChild(<Example />)
+  
   const self = <div class={main}>
     {container}
   </div>
