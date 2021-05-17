90e979206e4cf96c2753b1832389ff26ba1d6d29
Momijiichigo <ichigomomiji436@gmail.com>
2021-05-18 01:17:22 +0900

    make AttrHolder available in editor

--- src/index.jsx
+++ src/index.jsx
@@ -7,10 +7,11 @@ import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
 import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
 import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
-import { useAttr } from '@vanillajsx/vjsx'
+import { useAttr, AttrHolder } from '@vanillajsx/vjsx'
 
 globalThis.VJSX = VJSX
 globalThis.useAttr = useAttr
+globalThis.AttrHolder = AttrHolder
 
 self.MonacoEnvironment = {
   getWorker(_, label) {
