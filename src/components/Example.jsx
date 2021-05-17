2a2b0bd0377cb4648f84024faaf4524fc857b970
Momijiichigo <ichigomomiji436@gmail.com>
2021-05-17 21:36:20 +0900

    format

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -1,16 +1,16 @@
 import { useAttr } from '@vanillajsx/vjsx'
 
 //takes in attributes as arguments (access to children elements via 'children' attribute)
-const Example = ({ progValue = 0, children = null })=>{
+const Example = ({ progValue = 0, children = null }) => {
 
   //declare elements
-  const progress = <progress max={100} value={progValue}/>
+  const progress = <progress max={100} value={progValue} />
   const btn = <button>click</button>
   const progText = new Text()
   const self = (
     <div class='t3'>
       {btn}
-      {progress} 
+      {progress}
       {progText}%
       {children}
     </div>
@@ -25,22 +25,22 @@ const Example = ({ progValue = 0, children = null })=>{
 
   // functionalities
   //when `self.progValue` changed, run the following listener
-  self.watch('progValue', v=>{
+  self.watch('progValue', v => {
     progress.value = v
     progText.data = v
   })
 
-  btn.onclick = () =>{
+  btn.onclick = () => {
     /*
       below just looks assigning a value to a property,
       however this is running getter/setter method,
       which executes all registered listener functions via `watch` method.
     */
-    if(self.progValue<100) self.progValue+=10
+    if (self.progValue < 100) self.progValue += 10
     else self.progValue = 0
   }
 
   // return self element
-  return self	
+  return self
 }
 export default Example
