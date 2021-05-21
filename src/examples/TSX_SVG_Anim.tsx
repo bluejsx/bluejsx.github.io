a77dcdadd42f10e62f48f9adca147d19329fcb83
Momijiichigo <ichigomomiji436@gmail.com>
2021-05-21 17:24:48 +0900

    up

--- src/examples/TSX_SVG_Anim.tsx
+++ src/examples/TSX_SVG_Anim.tsx
@@ -19,18 +19,29 @@ const Example = () => {
         <circle vector-effect="non-scaling-stroke" cx="128" cy="128" r="128" fill="url(#_lgradient_2)"/>
         <line ref={[refs, 'line1']} x1='128' y1='223' x2='25' y2='71' stroke="white" stroke-linecap="round" />
         <line ref={[refs, 'line2']} x1='128' y1='223' x2='231' y2='71' stroke="white" stroke-linecap="round" />
-        <line ref={[refs, 'line3']} x2='64' y2='33' x1='192' y1='222' stroke="white" stroke-linecap="round" />
-        <line ref={[refs, 'line4']} x2='192' y2='33' x1='64' y1='222' stroke="white" stroke-linecap="round" />
+        <line ref={[refs, 'line3']} x1='192' y1='222' x2='64' y2='33' stroke="white" stroke-linecap="round" />
+        <line ref={[refs, 'line4']} x1='64' y1='222' x2='192' y2='33' stroke="white" stroke-linecap="round" />
       </g>
     </svg>
   )
   const { stopColor, line1, line2, line3, line4 } = refs
-  const duration = 1500
-  const lineAnimSetting: Parameters<typeof line1.animate> = [
+  const duration = 1200
+  const vLineAnimSetting: Parameters<typeof line1.animate> = [
     [
       {
         strokeDasharray: '0 100%',
         strokeWidth: 0,
+        easing: 'steps(1, end)',
+      },
+      {
+        strokeDasharray: '0 100%',
+        strokeWidth: 8,
+        offset: 0.1
+      },
+      {
+        strokeDasharray: '100% 0',
+        strokeWidth: 8,
+        offset: 0.7
       },
       {
         strokeDasharray: '100% 0',
@@ -44,10 +55,34 @@ const Example = () => {
       direction: 'alternate',
     }
   ]
-  line1.animate(...lineAnimSetting).play()
-  line2.animate(...lineAnimSetting).play()
-  line3.animate(...lineAnimSetting).play()
-  line4.animate(...lineAnimSetting).play()
+  const xLineAnimSetting: Parameters<typeof line1.animate> = [
+    [
+      {
+        strokeDasharray: '0 100%',
+        strokeWidth: 0,
+        easing: 'steps(1, end)',
+      },
+      {
+        strokeDasharray: '0 100%',
+        strokeWidth: 8,
+        offset: 0.5,
+      },
+      {
+        strokeDasharray: '100% 0',
+        strokeWidth: 8,
+      },
+    ],
+    {
+      duration,
+      easing: 'ease-in-out',
+      iterations: Infinity,
+      direction: 'alternate',
+    }
+  ]
+  line1.animate(...vLineAnimSetting).play()
+  line2.animate(...vLineAnimSetting).play()
+  line3.animate(...xLineAnimSetting).play()
+  line4.animate(...xLineAnimSetting).play()
   
   stopColor.animate([
     {
