675119925d0fab9e3332cf2ff7882dfb1b529c56
Momijiichigo <ichigomomiji436@gmail.com>
2021-10-17 08:03:05 +0000

    [skip ci] Updates

---
+++ assets/TSXAttrHolder.4a09e601.js
@@ -0,0 +1,40 @@
+import"./vendor.cdaf7cde.js";var t=`import { useAttr, AttrHolder, RefType } from 'bluejsx'
+
+//takes in attributes as arguments (access to children elements via 'children' attribute)
+const Example = ({ progValue = 0, children = null }) => {
+  //create state holder
+  const state = new AttrHolder()
+  //declare elements
+  const refs: RefType<{
+    btn: 'button'
+    progress: 'progress'
+  }> = {}
+  const progText = new Text()
+  const self = (
+    <div>
+      <button ref={[refs, 'btn']}>click</button>
+      <progress ref={[refs, 'progress']} max={100} value={progValue} />
+      {progText}%
+      {children}
+    </div>
+  )
+  const { btn, progress } = refs
+
+  //create state
+  useAttr(state, 'progValue', progValue)
+
+  // when \`state.progValue\` changes, run the following listener
+  state.watch('progValue', v => {
+    progress.value = v
+    progText.data = v
+  })
+
+  btn.onclick = () => {
+    if (state.progValue < 100) state.progValue += 10
+    else state.progValue = 0
+  }
+
+  // return self element
+  return self
+}
+export default Example`;export{t as default};
--- assets/index.d.710383a2.js
+++ assets/index.d.710383a2.js
@@ -1,1 +1,123 @@
-var e="interface jsxProps {\n    [key: string]: any;\n}\ndeclare type AdditionalElementProps = {\n    on: typeof EventTarget.prototype.addEventListener;\n    watch: (name: string, listener: (value: any) => void) => void;\n    _vf: {\n        [key: string]: Function[] & {\n            value?: any;\n        };\n    };\n} & {\n    [key in PropertyKey]: any;\n};\ndeclare type JSXElement = Element & AdditionalElementProps;\ndeclare type childFunc = (element?: JSXElement) => void;\ndeclare type HTMLTagName = keyof HTMLElementTagNameMap;\ndeclare type SVGTagName = keyof SVGElementTagNameMap;\ndeclare type JSXChildren = (JSXElement | string | childFunc | JSXChildren)[];\ndeclare type BlueHTMLAttrs<Element> = Partial<Element> | {\n    class?: string;\n    children?: JSXChildren;\n    ref?: [object, string];\n    style?: string;\n    [key: string]: any;\n};\ndeclare type BlueSVGAttrs<Element> = {\n    [key in keyof Element]?: string;\n} | {\n    class?: string;\n    children?: JSXChildren;\n    ref?: [object, string];\n    style?: string;\n    [key: string]: any;\n};\ndeclare type JSXElementTags = {\n    [key in keyof HTMLElementTagNameMap]: HTMLElementTagNameMap[key] & AdditionalElementProps;\n} & {\n    [key in keyof SVGElementTagNameMap]: SVGElementTagNameMap[key] & AdditionalElementProps;\n};\ndeclare type JSXElementTagNames = keyof JSXElementTags;\ndeclare global {\n    namespace Blue {\n        namespace JSX {\n            type Element = (HTMLElement | SVGElement) & AdditionalElementProps;\n            type IntrinsicElements = {\n                [key in keyof HTMLElementTagNameMap]: BlueHTMLAttrs<HTMLElementTagNameMap[key]>;\n            } & {\n                [key in keyof SVGElementTagNameMap]: BlueSVGAttrs<SVGElementTagNameMap[key]>;\n            };\n        }\n    }\n}\n\n/**\n * An object class which can be used with useAttr\n */\ndeclare class AttrHolder {\n    _vf: {\n        [key: string]: Function[] & {\n            value?: any;\n        };\n    };\n    constructor();\n    watch(name: string, listener: (value: any) => void): void;\n}\n/**\n *\n * @param target Your BlueJSX element or AttrHolder object.\n * @param propName Name of the property which you are defining.\n * @param defaultValue Set your default value.\n */\ndeclare function useAttr<Obj extends AttrHolder, PropName extends string, AttrType>(target: Obj, propName: PropName, defaultValue: AttrType): asserts target is Obj & Record<PropName, AttrType>;\n\ndeclare function render<T extends HTMLTagName>(component: T, props: jsxProps, ...children: JSXChildren): HTMLElementTagNameMap[T] & AdditionalElementProps;\ndeclare function render<T extends SVGTagName>(component: T, props: jsxProps, ...children: JSXChildren): SVGElementTagNameMap[T] & AdditionalElementProps;\ndeclare function render<T extends (...args: any) => any>(component: T, props: jsxProps, ...children: JSXChildren): ReturnType<typeof component>;\ndeclare function render<T extends Function>(component: T, props: jsxProps, ...children: JSXChildren): T[\"prototype\"];\ndeclare const Blue: {\n    r: typeof render;\n    Fragment: ({ children }: {\n        children: Element[];\n    }) => Element[];\n};\n\n/** Type for specific BlueJSX elements.\n * Usage:\n * ```ts\n * const d = <div /> as ElemType<'div'>\n * ```\n * */\ndeclare type ElemType<tagName extends JSXElementTagNames> = JSXElementTags[tagName];\n/**\n * A type for reference object.\n *\n * usage:\n * ```ts\n * const refs: RefType<{\n *  elem1: 'button'  //element tag name\n *  elem2: typeof FuncComponent  //function component\n *  elem3: ClassComponent //Custom Element (extends HTMLElement)\n * }> = {}\n * ```\n */\ndeclare type RefType<M extends {\n    [name: string]: (JSXElementTagNames | HTMLElement | Function | string);\n}> = {\n    [key in keyof M]?: M[key] extends JSXElementTagNames ? ElemType<M[key]> : M[key] extends HTMLElement ? M[key] : M[key] extends ((...args: any) => any) ? ReturnType<M[key]> : Blue.JSX.Element;\n};\n/**\n * usage:\n * ```ts\n * const Component = (\n *  {attrA}: FuncCompParam<{attrA?: string}>\n * ) => <div />\n * ```\n */\ndeclare type FuncCompParam<Param> = {\n    children?: [Blue.JSX.Element];\n} & Param;\n\nexport { AttrHolder, ElemType, FuncCompParam, RefType, Blue as default, useAttr };\n";export{e as default};
+var e=`interface jsxProps {
+    [key: string]: any;
+}
+declare type AdditionalElementProps = {
+    on: typeof EventTarget.prototype.addEventListener;
+    watch: (name: string, listener: (value: any) => void) => void;
+    _vf: {
+        [key: string]: Function[] & {
+            value?: any;
+        };
+    };
+} & {
+    [key in PropertyKey]: any;
+};
+declare type JSXElement = Element & AdditionalElementProps;
+declare type childFunc = (element?: JSXElement) => void;
+declare type HTMLTagName = keyof HTMLElementTagNameMap;
+declare type SVGTagName = keyof SVGElementTagNameMap;
+declare type JSXChildren = (JSXElement | string | childFunc | JSXChildren)[];
+declare type BlueHTMLAttrs<Element> = Partial<Element> | {
+    class?: string;
+    children?: JSXChildren;
+    ref?: [object, string];
+    style?: string;
+    [key: string]: any;
+};
+declare type BlueSVGAttrs<Element> = {
+    [key in keyof Element]?: string;
+} | {
+    class?: string;
+    children?: JSXChildren;
+    ref?: [object, string];
+    style?: string;
+    [key: string]: any;
+};
+declare type JSXElementTags = {
+    [key in keyof HTMLElementTagNameMap]: HTMLElementTagNameMap[key] & AdditionalElementProps;
+} & {
+    [key in keyof SVGElementTagNameMap]: SVGElementTagNameMap[key] & AdditionalElementProps;
+};
+declare type JSXElementTagNames = keyof JSXElementTags;
+declare global {
+    namespace Blue {
+        namespace JSX {
+            type Element = (HTMLElement | SVGElement) & AdditionalElementProps;
+            type IntrinsicElements = {
+                [key in keyof HTMLElementTagNameMap]: BlueHTMLAttrs<HTMLElementTagNameMap[key]>;
+            } & {
+                [key in keyof SVGElementTagNameMap]: BlueSVGAttrs<SVGElementTagNameMap[key]>;
+            };
+        }
+    }
+}
+
+/**
+ * An object class which can be used with useAttr
+ */
+declare class AttrHolder {
+    _vf: {
+        [key: string]: Function[] & {
+            value?: any;
+        };
+    };
+    constructor();
+    watch(name: string, listener: (value: any) => void): void;
+}
+/**
+ *
+ * @param target Your BlueJSX element or AttrHolder object.
+ * @param propName Name of the property which you are defining.
+ * @param defaultValue Set your default value.
+ */
+declare function useAttr<Obj extends AttrHolder, PropName extends string, AttrType>(target: Obj, propName: PropName, defaultValue: AttrType): asserts target is Obj & Record<PropName, AttrType>;
+
+declare function render<T extends HTMLTagName>(component: T, props: jsxProps, ...children: JSXChildren): HTMLElementTagNameMap[T] & AdditionalElementProps;
+declare function render<T extends SVGTagName>(component: T, props: jsxProps, ...children: JSXChildren): SVGElementTagNameMap[T] & AdditionalElementProps;
+declare function render<T extends (...args: any) => any>(component: T, props: jsxProps, ...children: JSXChildren): ReturnType<typeof component>;
+declare function render<T extends Function>(component: T, props: jsxProps, ...children: JSXChildren): T["prototype"];
+declare const Blue: {
+    r: typeof render;
+    Fragment: ({ children }: {
+        children: Element[];
+    }) => Element[];
+};
+
+/** Type for specific BlueJSX elements.
+ * Usage:
+ * \`\`\`ts
+ * const d = <div /> as ElemType<'div'>
+ * \`\`\`
+ * */
+declare type ElemType<tagName extends JSXElementTagNames> = JSXElementTags[tagName];
+/**
+ * A type for reference object.
+ *
+ * usage:
+ * \`\`\`ts
+ * const refs: RefType<{
+ *  elem1: 'button'  //element tag name
+ *  elem2: typeof FuncComponent  //function component
+ *  elem3: ClassComponent //Custom Element (extends HTMLElement)
+ * }> = {}
+ * \`\`\`
+ */
+declare type RefType<M extends {
+    [name: string]: (JSXElementTagNames | HTMLElement | Function | string);
+}> = {
+    [key in keyof M]?: M[key] extends JSXElementTagNames ? ElemType<M[key]> : M[key] extends HTMLElement ? M[key] : M[key] extends ((...args: any) => any) ? ReturnType<M[key]> : Blue.JSX.Element;
+};
+/**
+ * usage:
+ * \`\`\`ts
+ * const Component = (
+ *  {attrA}: FuncCompParam<{attrA?: string}>
+ * ) => <div />
+ * \`\`\`
+ */
+declare type FuncCompParam<Param> = {
+    children?: [Blue.JSX.Element];
+} & Param;
+
+export { AttrHolder, ElemType, FuncCompParam, RefType, Blue as default, useAttr };
+`;export{e as default};
