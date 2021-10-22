1bea871350b867386976616efb558f7c880d6414
Momijiichigo <ichigomomiji436@gmail.com>
2021-10-21 19:27:14 -0700

    delete md loader bc it's nolonger needed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import withPages from 'vite-with-blue-pages'
 //import withPages from 'AA/lib/index.cjs';
-import mdLoader from './mdloader'
 import bundleWorker from './bundleWorker';
 /** @type {import('vite').UserConfig} */
 export default withPages({
   plugins: [
-    mdLoader(),
     bundleWorker(),
   ],
   base: './',
