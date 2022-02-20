cd088225d62e4feef2d82778256dc5d9d9b2a08b
Momijiichigo <46286940+Momijiichigo@users.noreply.github.com>
2022-02-20 23:50:36 +0000

    Deploying to gh-pages from @ bluejsx/bluejsx.github.io@615f059751673e9a350c122bc365dafee6b5f539 🚀

---
+++ assets/TSXWithRef.6c1fa226.js
@@ -0,0 +1,42 @@
+import"./vendor.9c9290dc.js";var n=`import { useAttr, RefType, FuncCompParam } from 'bluejsx'
+
+//takes in attributes as arguments (access to children elements via 'children' attribute)
+const Example = ({ progValue = 0, children }: FuncCompParam<{ progValue: number }>) => {
+
+  //declare elements
+  const refs: RefType<{
+    btn: 'button'
+    progress: 'progress'
+  }> = {}
+  const progText = new Text()
+  const self = (
+    <div>
+      <button ref={[refs, 'btn']}>click</button>
+      <progress ref={[refs, 'progress']} max={100} value={progValue} />
+      {progText}%
+      {children}
+    </div>
+  )
+  const { btn, progress } = refs
+  /*
+    below defines a property named 'progValue',
+    and when 'progValue' changes, 
+    all registered listeners will be executed.
+  */
+  useAttr(self, 'progValue', progValue)
+
+  //when \`self.progValue\` changes, run the following listener
+  self.watch('progValue', v => {
+    progress.value = v
+    progText.data = v
+  })
+
+  btn.onclick = () => {
+    if (self.progValue < 100) self.progValue += 10
+    else self.progValue = 0
+  }
+
+  // return self element
+  return self
+}
+export default Example`;export{n as default};
